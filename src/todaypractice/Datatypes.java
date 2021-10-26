6745ba2dbc2e4b5d75dd2cf9919d1e750ded6cc2
SK <SK@192.168.0.107>
2021-10-26 09:21:46 +0530

    testing conflicts

--- src/todaypractice/Datatypes.java
+++ src/todaypractice/Datatypes.java
@@ -3,7 +3,7 @@ package todaypractice;
 public class Datatypes {
 
 	public static void main(String[] args) {
-		char myvar1 = 65, myvar2 = 66, myvar3 = 67;
+		char myva=77, myvar1 = 65, myvar2 = 66, myvar3 = 67;
 		System.out.println(myvar1);
 		System.out.println(myvar2);
 		System.out.println(myvar3);
